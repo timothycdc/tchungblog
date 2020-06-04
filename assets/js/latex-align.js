30091798195bf910e17dbfc70b75cb572dd956d2
timothycdc <timothycdc@gmail.com>
2020-06-04 18:51:03 +0800

    Update latex-align.js

--- assets/js/latex-align.js
+++ assets/js/latex-align.js
@@ -12,7 +12,7 @@
           if (parent.childNodes.length != 1) continue;
           parent.firstChild.style.border = 'none';
         }
-        if (parent.nodeName === 'P') parent.style.textAlign = 'center';
+        if (parent.nodeName === 'P') parent.style.textAlign = 'left';
       }
     }
   }
